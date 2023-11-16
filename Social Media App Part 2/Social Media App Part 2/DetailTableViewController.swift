f784d992d617ab06d72d3a3e13be8cfe1f9e2b4f
colinerickson <142511094+colinerickson@users.noreply.github.com>
2023-11-16 09:53:32 -0700

    Add files via upload

---
+++ Social Media App Part 2/Social Media App Part 2/DetailTableViewController.swift
@@ -0,0 +1,113 @@
+//
+//  DetailTableViewController.swift
+//  Social Media App Part 2
+//
+//  Created by Colin Erickson on 11/15/23.
+//
+
+import UIKit
+
+class DetailTableViewController: UITableViewController {
+    var posts: [Post] = [
+        Post(title: "First post!", bodyText: "Hi guys, this is my first post!! I wanted to share my thoughts and experiences with you. It's amazing to be part of this community. Looking forward to connecting with all of you!", date: "12/25/2017", user: "Gerald997", comments: 3),
+        Post(title: "I went to Switzerland.", bodyText: "Took a quick trip to Switzerland. The breathtaking landscapes and picturesque views left me in awe. I explored charming villages and indulged in delicious Swiss chocolate. Can't wait to go back and experience more!", date: "2/5/23", user: "Traveler23", comments: 7),
+        Post(title: "Exploring the Mountains", bodyText: "Hiking in the Alps was an amazing experience! The fresh mountain air, stunning vistas, and challenging trails made it a memorable adventure. I discovered hidden waterfalls and enjoyed the serenity of nature.", date: "3/10/23", user: "NatureExplorer", comments: 12),
+        Post(title: "Cooking Adventures", bodyText: "Tried a new recipe today and it turned out delicious! Cooking is my passion, and experimenting with flavors brings me joy. The kitchen is my creative space, and I love sharing my culinary creations with others.", date: "4/22/23", user: "ChefLife", comments: 5),
+        Post(title: "Coding Journey", bodyText: "Started learning Swift programming. It's challenging but fun! Coding opens up a world of possibilities, and I'm excited to build amazing apps. The Swift language is elegant, and the logic behind programming fascinates me.", date: "5/15/23", user: "CodeNewbie", comments: 2),
+        Post(title: "Beach Sunset", bodyText: "Enjoying a beautiful sunset by the ocean. The colors in the sky are mesmerizing, casting a warm glow over the waves. It's a moment of tranquility, a perfect time to reflect and appreciate the beauty of nature.", date: "6/8/23", user: "SunsetLover", comments: 8),
+        Post(title: "Book Recommendations", bodyText: "Just finished an amazing book! Any recommendations for the next one? Reading is my escape, and each book takes me on a new adventure. Share your favorite reads and let's explore new literary worlds together.", date: "7/1/23", user: "Bookworm", comments: 4),
+        Post(title: "Fitness Challenge", bodyText: "Completed my 30-day fitness challenge. Feeling accomplished! The journey to a healthier lifestyle is rewarding. Consistency is key, and each workout brings me closer to my fitness goals. Who's up for the next challenge?", date: "8/12/23", user: "FitLife", comments: 6),
+        Post(title: "Weekend Getaway", bodyText: "Spent the weekend in a cozy cabin in the woods. Surrounded by nature, the tranquility was rejuvenating. Long walks, campfires, and starry nights made it a perfect escape. Already planning the next adventure!", date: "9/4/23", user: "WeekendExplorer", comments: 10),
+        Post(title: "Gaming Marathon", bodyText: "Had an epic gaming marathon with friends. Victory Royale! Gaming is not just a hobby; it's a passion. The thrill of competition, teamwork, and the joy of victory make it an unforgettable experience. Ready for the next challenge!", date: "10/17/23", user: "Gamer123", comments: 15)
+    ]
+
+    var selectedPost: Post?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return posts.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postID", for: indexPath) as! PostTableViewCell
+        let post = posts[indexPath.row]
+        cell.titleLabel.text = post.title
+        cell.bodyTextView.text = post.bodyText
+        cell.commentsLabel.text = "\(post.comments)"
+        cell.userLabel.text = post.user
+        cell.dateLabel.text = post.date
+        cell.bodyTextView.isEditable = false
+        
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "postID", for: indexPath)
+        return 250
+    }
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Social Media App Part 2/Social Media App Part 2/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  Social Media App Part 2
+//
+//  Created by Colin Erickson on 11/15/23.
+//
+
+import Foundation
+
+
+struct Post {
+    var title: String
+    var bodyText: String
+    var date: String
+    var user: String
+    var comments: Int
+}
